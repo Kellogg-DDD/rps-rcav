16096407b89ad496dd7e8c236cb9e3749a67758a
mhemani7 <malvikhemani@gmail.com>
2020-05-07 22:09:33 +0000

    Final Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,56 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def rules
+    render({ :template => "game_templates/rules.html.erb" })
+  end
+  
+  def play_rock
+    moves = ["rock", "paper", "scissors"]
+    
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    moves = ["rock", "paper", "scissors"]
+    
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    moves = ["rock", "paper", "scissors"]
+    
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  
+  get("/rock", { :controller => "application", :action => "play_rock" })
+
+  get("/paper", { :controller => "application", :action => "play_paper" })
+
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
+
+  get("/", { :controller => "application", :action => "rules" })
+
+  get("/rules", { :controller => "application", :action => "rules" })
 
   # Write your routes above this line
   # =================================
